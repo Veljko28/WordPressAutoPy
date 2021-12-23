bfe8e37e4ad3278b2d96cd87d046f9b4b7cccc26
Veljko28 <charlibear28@gmail.com>
2021-12-23 18:18:07 +0100

    adding metle from savacoop

---
+++ metle.py
@@ -0,0 +1,8 @@
+from savatemplate import template
+
+if __name__ == "__main__":
+    section = "https://savacoop.rs/sr/category/veleprodaja/kuca-i-basta/program-za-kucu/metle-mopovi-i-pajalice"
+    scroll = 0
+    items = 25
+    template(section, scroll, items)
+
