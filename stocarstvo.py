09d488ffc9ad4080f8dc3d2600fa8d612b672777
Veljko28 <charlibear28@gmail.com>
2021-12-23 18:18:46 +0100

    adding stocarstvo from savacoop

---
+++ stocarstvo.py
@@ -0,0 +1,8 @@
+from savatemplate import template
+
+if __name__ == "__main__":
+    section = "https://savacoop.rs/sr/category/veleprodaja/zivotinje-i-ljubimci/stocarstvo"
+    scroll = 5
+    items = 79
+    template(section, scroll, items)
+
