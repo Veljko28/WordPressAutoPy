d9c335a332b8325b534ad88a35079e7e4d156a69
Veljko28 <charlibear28@gmail.com>
2021-12-24 22:00:45 +0100

    adding slavine from savacoop

---
+++ slavine.py
@@ -0,0 +1,8 @@
+from savatemplate import template
+
+if __name__ == "__main__":
+    section = "https://savacoop.rs/sr/category/veleprodaja/oprema-za-navodnjavanje-i-povrtarstvo/navodnjavanje/slavine"
+    scroll = 5
+    items = 67
+    template(section, scroll, items)
+
