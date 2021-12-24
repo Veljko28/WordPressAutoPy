d076df0e3b2770a74e54841a0f0549ea9f25d38e
Veljko28 <charlibear28@gmail.com>
2021-12-24 22:00:11 +0100

    adding pcelarstvo from savacoop

---
+++ pcelarstvo.py
@@ -0,0 +1,8 @@
+from savatemplate import template
+
+if __name__ == "__main__":
+    section = "https://savacoop.rs/sr/category/veleprodaja/zivotinje-i-ljubimci/pcelarstvo"
+    scroll = 4
+    items = 61
+    template(section, scroll, items)
+
