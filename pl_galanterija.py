5b3bfcb9289a6e0a2aef4214f10293cac7a1a15a
Veljko28 <charlibear28@gmail.com>
2021-12-24 22:00:29 +0100

    adding pl galenterija from savacoop

---
+++ pl_galanterija.py
@@ -0,0 +1,8 @@
+from savatemplate import template
+
+if __name__ == "__main__":
+    section = "https://savacoop.rs/sr/category/veleprodaja/kuca-i-basta/program-za-kucu/plasticna-galanterija"
+    scroll = 5
+    items = 67
+    template(section, scroll, items)
+
