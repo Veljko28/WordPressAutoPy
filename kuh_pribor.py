eacdeb26bc7dffdce61adcacc2fa72dd1f017ba0
Veljko28 <charlibear28@gmail.com>
2021-12-24 21:59:42 +0100

    adding kuhinjski pribor from savacoop

---
+++ kuh_pribor.py
@@ -0,0 +1,8 @@
+from savatemplate import template
+
+if __name__ == "__main__":
+    section = "https://savacoop.rs/sr/category/veleprodaja/kuca-i-basta/program-za-kucu/kuhinjski-pribor-i-oprema"
+    scroll = 1
+    items = 22
+    template(section, scroll, items)
+
