66dccd8929c884a3394925bcc58e28673fe9e736
Veljko28 <charlibear28@gmail.com>
2021-12-29 22:12:53 +0100

    adding updates to old files

--- slika.py
+++ slika.py
@@ -1,11 +1,13 @@
 import cv2
 import numpy as np
+import uuid
+import os
 
 # Pomocna funkcija koja sacuva sliku
 def getImage(url, driver):
     driver.get(url)
 
-    imageName = url.split('/')[-1]
+    imageName = str(uuid.uuid4()) + ".png"
 
     with open(imageName, 'wb') as file:
         file.write(driver.find_element_by_tag_name("img").screenshot_as_png)
@@ -19,12 +21,13 @@ def removeImageBorder(imagePath):
     cv2.imwrite(imagePath, crop_img)
 
 def resizeImage(imagePath):
-    removeImageBorder(imagePath)
+    #removeImageBorder(imagePath)
     img = cv2.imread(imagePath)
     ht, wd, cc = img.shape
-    ww = 800
-    hh = 800
-    color = (255, 255, 255)
+    ww = 1000
+    hh = 1000
+    b, g, r = (img[0, 0])
+    color = (r, g, b)
     result = np.full((hh, ww, cc), color, dtype=np.uint8)
 
     xx = (ww - wd) // 2
@@ -32,3 +35,12 @@ def resizeImage(imagePath):
     result[yy:yy + ht, xx:xx + wd] = img
 
     cv2.imwrite(imagePath, result)
+
+def imageClear():
+    for file in os.listdir('C:\\Users\\Sasha\\Desktop\\Artisoft\\pyTest\\MoneyMaker'):
+        if file.endswith('.png') or file.endswith('.jpg') or file.endswith('.jpeg'):
+            os.remove(file)
+
+
+if __name__ == "__main__":
+    imageClear()
