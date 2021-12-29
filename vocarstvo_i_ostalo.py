c78a71825b02764a5ee43d8794e1331170aba482
Veljko28 <charlibear28@gmail.com>
2021-12-29 22:11:26 +0100

    adding vocastvo i ostalo from savacoop

---
+++ vocarstvo_i_ostalo.py
@@ -0,0 +1,8 @@
+from savatemplate import template
+
+if __name__ == "__main__":
+    section = "https://savacoop.rs/sr/category/veleprodaja/oprema-za-navodnjavanje-i-povrtarstvo/oprema-za-vocarstvo-povrtarstvo-i-hortikulturu"
+    scroll = 15
+    items = 286
+    template(section, scroll, items)
+
