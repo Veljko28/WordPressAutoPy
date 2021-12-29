c4d0802ab986dbfb7fcb712ace2d0921900f1cc0
Veljko28 <charlibear28@gmail.com>
2021-12-29 22:09:54 +0100

    adding kapljaci from savacoop

---
+++ kapljaci.py
@@ -0,0 +1,8 @@
+from savatemplate import template
+
+if __name__ == "__main__":
+    section = "https://savacoop.rs/sr/category/veleprodaja/oprema-za-navodnjavanje-i-povrtarstvo/navodnjavanje/kapljaci"
+    scroll = 0
+    items = 10
+    template(section, scroll, items)
+
