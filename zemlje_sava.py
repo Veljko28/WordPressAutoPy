6981c9abc7fd04d7d10242dc5a4b8500edf673c4
Veljko28 <charlibear28@gmail.com>
2021-12-31 19:10:03 +0100

    adding zemlja from savacoop

---
+++ zemlje_sava.py
@@ -0,0 +1,8 @@
+from savatemplate import template
+
+if __name__ == "__main__":
+    section = "https://savacoop.rs/sr/category/veleprodaja/kuca-i-basta/zemlje-i-substrati"
+    scroll = 2
+    items = 36
+    template(section, scroll, items)
+
