89e7d849f74e34c7ef07c0d31668b540aa8f34d3
Veljko28 <charlibear28@gmail.com>
2022-01-29 13:10:56 +0100

    adding sekacice from agrimarket

---
+++ sekacice.py
@@ -0,0 +1,7 @@
+from agritemplate import template
+
+# gotovo
+if __name__ == "__main__":
+    section = "https://shop.agromarket.rs/seckalice-i-drobilice-za-grane/"
+    pages = 1
+    template(section, pages)
