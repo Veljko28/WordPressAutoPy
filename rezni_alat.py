8cca87e25ba409f15b9e241bc0e35fa4251e6ba8
Veljko28 <charlibear28@gmail.com>
2022-01-29 13:10:38 +0100

    adding rezni alat from agromarket

---
+++ rezni_alat.py
@@ -0,0 +1,7 @@
+from agritemplate import template
+
+# gotovo
+if __name__ == "__main__":
+    section = "https://shop.agromarket.rs/rezni-alat-i-oprema/"
+    pages = 5
+    template(section, pages)
