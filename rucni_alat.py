573533643faa864d8627c215a46078da29843ff1
Veljko28 <charlibear28@gmail.com>
2022-01-29 13:10:17 +0100

    adding rucni alat from agrimarket

---
+++ rucni_alat.py
@@ -0,0 +1,7 @@
+from agritemplate import template
+
+# gotovo
+if __name__ == "__main__":
+    section = "https://shop.agromarket.rs/rucni-bastenski-alat/"
+    pages = 7
+    template(section, pages)
