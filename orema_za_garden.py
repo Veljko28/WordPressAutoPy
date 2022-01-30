89efef3e58d00454173f5fa0ff944d20f68b1dfc
Veljko28 <charlibear28@gmail.com>
2022-01-30 19:36:26 +0100

    adding oprema za garden from agrimarket

---
+++ orema_za_garden.py
@@ -0,0 +1,7 @@
+from agritemplate import template
+
+# gotovo
+if __name__ == "__main__":
+    section = "https://shop.agromarket.rs/oprema-za-garden-masine/"
+    pages = 2
+    template(section, pages)
