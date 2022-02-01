1bd54ef7db331a67286734d364530436fa6aea3c
Veljko28 <charlibear28@gmail.com>
2022-02-01 22:40:24 +0100

    adding akumulatorski trimeri from agromarket

---
+++ akumulatorski.py
@@ -0,0 +1,7 @@
+from agritemplate import template
+
+# gotovo
+if __name__ == "__main__":
+    section = "https://shop.agromarket.rs/akumulatorski-alat-oregon-power-on/"
+    pages = 1
+    template(section, pages)
