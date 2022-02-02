00095e853f93bdcd6d64f39c0d4a868137ce8ce0
Veljko28 <charlibear28@gmail.com>
2022-02-02 22:22:03 +0100

    adding potrosni materijal from agromarket

---
+++ potrosni_materijal.py
@@ -0,0 +1,7 @@
+from agritemplate import template
+
+# gotovo
+if __name__ == "__main__":
+    section = "https://shop.agromarket.rs/potrosni-materijal-za-garden-masine/"
+    pages = 6
+    template(section, pages)
