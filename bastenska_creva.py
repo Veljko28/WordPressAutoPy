6e81fe87901ca28da4fe83366f94b0529dc44983
Veljko28 <charlibear28@gmail.com>
2022-02-06 21:49:34 +0100

    adding bastenska creva from agrimarket

---
+++ bastenska_creva.py
@@ -0,0 +1,7 @@
+from agritemplate import template
+
+# gotovo
+if __name__ == "__main__":
+    section = "https://shop.agromarket.rs/bastenska-creva-i-oprema/"
+    pages = 3
+    template(section, pages)
