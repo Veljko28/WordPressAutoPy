6b8586d02f50f0ebf62fa4483d83605b6807c4ec
Veljko28 <charlibear28@gmail.com>
2022-02-27 20:27:00 +0100

    adding agri image correction

---
+++ slika_agri.py
@@ -0,0 +1,48 @@
+import cv2
+import numpy as np
+import uuid
+import os
+
+# Pomocna funkcija koja sacuva sliku
+def getImage(url, driver):
+    driver.get(url)
+
+    imageName = str(uuid.uuid4()) + ".png"
+
+    with open(imageName, 'wb') as file:
+        file.write(driver.find_element_by_tag_name("img").screenshot_as_png)
+
+    return imageName
+
+
+def removeImageBorder(imagePath):
+    img = cv2.imread(imagePath)
+    h, w, c = img.shape
+    crop_img = img[5:h, 5:w]
+    cv2.imwrite(imagePath, crop_img)
+
+
+def resizeImage(imagePath):
+    removeImageBorder(imagePath)
+    img = cv2.imread(imagePath)
+    ht, wd, cc = img.shape
+    ww = 700
+    hh = 700
+    b, g, r = (img[0, 0])
+    color = (r, g, b)
+    result = np.full((hh, ww, cc), color, dtype=np.uint8)
+
+    xx = (ww - wd) // 2
+    yy = (hh - ht) // 2
+    result[yy:yy + ht, xx:xx + wd] = img
+
+    cv2.imwrite(imagePath, result)
+
+def imageClear():
+    for file in os.listdir('C:\\Users\\Sasha\\Desktop\\Artisoft\\pyTest\\MoneyMaker\\agrimarket'):
+        if file.endswith('.png') or file.endswith('.jpg') or file.endswith('.jpeg'):
+            os.remove(file)
+
+
+if __name__ == "__main__":
+    imageClear()
