1b51d073882412b867608eeadbe80b55c92494e7
Veljko28 <charlibear28@gmail.com>
2022-02-27 20:26:45 +0100

    adding pranje pod pritiskom from agrimarket

---
+++ pranje_pod_pritiskom.py
@@ -0,0 +1,7 @@
+from agritemplate import template
+
+# gotovo
+if __name__ == "__main__":
+    section = "https://shop.agromarket.rs/uredjaji-za-pranje-pod-pritiskom/"
+    pages = 1
+    template(section, pages)
